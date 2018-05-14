6fd259d26317c23adbe2420cfccc5a7e380ef120
clabroche <corentinlabroche@gmail.com>
2018-05-14 14:26:01 +0000

    demo on navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,45 @@
 import { Component } from '@angular/core';
 import { Configuration } from '../modules/core/sidebar/sidebar.component';
+import { SideBarService } from '../public_api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  conf: Configuration = {};
+  constructor(private sideBarService: SideBarService) {
+    sideBarService.open();
+    this.conf = {
+      /**
+       * List of all icons display on the main area
+       */
+      list: [
+        {
+          icon: 'fa fa-home',
+          description: 'Accueil',
+          click: function() {
+            console.log('hey');
+          }
+        }
+      ],
+      /**
+       * List of all icons display on the bottom area
+       */
+      bottom: [
+        {
+          icon: 'fa fa-cog',
+          description: 'Settings',
+          click: function () {
+            console.log('ho');
+          },
+          externalUrl: 'lkjelkjdelk'
+        }
+      ]
+    };
+  }
+  click($event) {
+    console.log($event);
+  }
+}
