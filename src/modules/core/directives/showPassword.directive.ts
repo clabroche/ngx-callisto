3a400b04efd3e7cc1b8bbfdf5ff31b1867618a23
clabroche <corentinlabroche@gmail.com>
2018-05-25 13:43:41 +0000

    [PATCH] fix show passord eye

--- src/modules/core/directives/showPassword.directive.ts
+++ src/modules/core/directives/showPassword.directive.ts
@@ -7,40 +7,45 @@ import { Directive, OnDestroy, Input, AfterViewChecked, ElementRef, Renderer2, O
  */
 @Directive({
   selector: '[show-password]',
-  host: {}
 })
-export class ShowPasswordDirective implements OnInit{
+export class ShowPasswordDirective implements OnInit {
 
   /**
    * Load some dependencies
    */
-  constructor(private renderer: Renderer2, private hostElement: ElementRef){}
-  
+  constructor(private renderer: Renderer2, private hostElement: ElementRef) {}
+
   ngOnInit(): void {
+    const container = this.renderer.createElement('div');
+    this.renderer.setStyle(container, 'position', 'relative');
     const div = this.renderer.createElement('i');
     this.renderer.addClass(div, 'fa');
     this.renderer.addClass(div, 'fa-eye-slash');
     this.renderer.addClass(div, 'toggle-eye');
-    this.renderer.setStyle(div, 'position', 'absolute')
-    this.renderer.setStyle(div, 'display', 'inherit')
-    this.renderer.setStyle(div, 'cursor', 'pointer')
+    this.renderer.setStyle(div, 'position', 'absolute');
+    this.renderer.setStyle(div, 'top', 0);
+    this.renderer.setStyle(div, 'right', 0);
+    this.renderer.setStyle(div, 'display', 'inherit');
+    this.renderer.setStyle(div, 'cursor', 'pointer');
+
     this.renderer.listen(div, 'click', event => {
-      const input = this.hostElement.nativeElement
-      if(input.type === 'password'){
-        input.type = 'text'
+      const input = this.hostElement.nativeElement;
+      if (input.type === 'password') {
+        input.type = 'text';
         this.renderer.addClass(div, 'fa-eye');
         this.renderer.removeClass(div, 'fa-eye-slash');
       } else {
-        input.type = 'password'
+        input.type = 'password';
         this.renderer.removeClass(div, 'fa-eye');
         this.renderer.addClass(div, 'fa-eye-slash');
       }
-    })
-    
-    this.renderer.setStyle(this.hostElement.nativeElement, 'padding-left', '2em')
-    
+    });
+
     const parent = this.hostElement.nativeElement.parentNode;
-    this.renderer.insertBefore(parent, div, this.hostElement.nativeElement)
+    this.renderer.insertBefore(parent, container, this.renderer.nextSibling(this.hostElement.nativeElement));
+    this.renderer.appendChild(container, this.hostElement.nativeElement);
+    this.renderer.appendChild(container, div);
+    // this.renderer.removeChild(parent,this.hostElement.nativeElement)
   }
 
 }
